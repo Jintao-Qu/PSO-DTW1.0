b0b627024812949462e0a18570c803e86c010c65
JinTao_Qu <35619744+Jintao-Qu@users.noreply.github.com>
2018-11-12 21:50:36 +0800

    Create utils.py

---
+++ code/utils.py
@@ -0,0 +1,20 @@
+def load_txt(filename):
+    v=[]
+    file=open(filename,'r')
+    for i in file.readlines():
+        #i = i.strip('\n')  # 除去换行
+        i = i.split(' ')  # 文件以“ ”分隔
+        #if "" in i:  # 解决每行结尾有空格的问题
+            #i.remove("")
+        v.append(eval(i[0]))
+    return v
+
+def main(filename):
+
+    v=load_txt(filename)
+    print(v)
+   # #print(len(v))
+
+if __name__ =='__main__':
+    main(filename="data/carcount.txt")
+
