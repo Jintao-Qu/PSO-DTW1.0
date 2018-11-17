69f7f91b6a673a7368c450feabe4fb53811c8889
Jintao_Qu <00002@protonmail.com>
2018-11-17 10:15:31 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -21,20 +21,18 @@ def main(filename, wmin, wmax, prng=None, display=False):
     ea.topology = inspyred.swarm.topologies.ring_topology
     final_pop = ea.evolve(generator=problem.generator,
                           evaluator=problem.evaluator,
-                          pop_size=100,
+                          pop_size=16,
                           bounder=problem.bounder,
                           maximize=problem.maximize,
-                          max_evaluations=300,
+                          max_evaluations=4800,
                           neighborhood_size=5)
     if display:
         best = max(final_pop)
-
         print('Best Solution: \n{0}'.format(str(best)))
-
         return ea
 
-if __name__ == '__main__':
-    main(filename="data/carcount.txt", wmin=5, wmax=7, display=True)
+#if __name__ == '__main__':
+#   main(filename="data/carcount.txt", wmin=5, wmax=7, display=True)
 
 
 
