434214769216c0773a4c94e07856baeaf74501d3
Jintao_Qu <00002@protonmail.com>
2018-11-24 00:08:12 +0800

    Update timeseriesproblem.py

--- code/timeseriesproblem.py
+++ code/timeseriesproblem.py
@@ -10,18 +10,18 @@ class timeseriesproblem(Benchmark):
         self.wmin = wmin
         self.wmax = wmax
         
-        self.bounder=bounder.bounder(len=self.len,lower_bound=[0, self.wmin, 0, self.wmin],
+        self.bounder = bounder.bounder(len=self.len,lower_bound=[0, self.wmin, 0, self.wmin],
                                   upper_bound=[self.len, self.wmax, self.len, self.wmax])
         self.maximize = False
         self.global_optimum = [0 for _ in range(self.dimensions)]
 
-    def generator(self,random,args):
-        tw1=random.randint(self.wmin,self.wmax)
-        tw2=random.randint(self.wmin,self.wmax)
-        twi=random.randint(0,self.len-tw1)
-        twj=random.randint(0,self.len-tw2)
-        #print("###",[twi,tw1,twj,tw2])
-        return [twi,tw1,twj,tw2]
+    def generator(self, random, args):
+        tw1 = random.randint(self.wmin, self.wmax)
+        tw2 = random.randint(self.wmin, self.wmax)
+        twi = random.randint(0, self.len-tw1)
+        twj = random.randint(0, self.len-tw2)
+
+        return [twi, tw1, twj, tw2]
 
     def evaluator(self, candidates, args):
         from numpy import array, zeros, argmin, inf, ndim
