a47d722d55ce7001683068048120a1f043d343c3
Jintao_Qu <00002@protonmail.com>
2018-11-24 00:07:11 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -5,17 +5,18 @@ from numpy.linalg import norm
 import timeseriesproblem
 from utils import load_txt
 import pso
-
+import globalvar as gl
 def main(filename, wmin, wmax, prng=None, display=False):
     if prng is None:
         prng = Random()
         prng.seed(time())
 
     v = load_txt(filename)
+    print("len_v: ", len(v))
 
     problem = timeseriesproblem.timeseriesproblem(dimensions=4, v=v, wmin=wmin, wmax=wmax,
                                                   dist=lambda x, y: norm(x - y, ord=1))
-
+    gl.set_value("problem", problem)
     ea = pso.pso(prng)
     ea.terminator = inspyred.ec.terminators.evaluation_termination
     ea.topology = inspyred.swarm.topologies.ring_topology
@@ -27,7 +28,11 @@ def main(filename, wmin, wmax, prng=None, display=False):
                           max_evaluations=4800,
                           neighborhood_size=5)
     if display:
-        best = max(final_pop)
+        if (gl.get_value("Elite") == False):
+            best = max(final_pop)
+        else:
+            el = gl.get_value("Elite_list")
+            best = max(el)
         print('Best Solution: \n{0}'.format(str(best)))
         return ea
 
