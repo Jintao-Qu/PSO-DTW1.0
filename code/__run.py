9e35f7a01bd42c9fbac89580ab36149a24c24e86
Jintao_Qu <00002@protonmail.com>
2018-11-24 00:06:42 +0800

    Update __run.py

--- code/__run.py
+++ code/__run.py
@@ -1,5 +1,6 @@
 from main import *
 import globalvar as gl
+
 from numpy import *
 import matplotlib
 from pylab import *
@@ -8,14 +9,18 @@ filename = "pic/pic_carcount.txt"
 gl._init()
 f = open(filename, "w")
 gl.set_value("f", f)
+gl.set_value("CRAZY_PSO", 0.002)#2e-3
+gl.set_value("Elite", True)
+gl.set_value("Elite_list", [])
 
 for i in range(10):
+   gl.set_value("Elite_list", [])
    f = open(filename, "w")
    gl.set_value("f", f)
    main(filename="data/carcount.txt", wmin=5, wmax=7, display=True)
    f.close()
    v=[]
-   rf=open(filename,'r')
+   rf=open(filename, 'r')
    for j in rf.readlines():
       v.append(eval(j))
    plot(v)
