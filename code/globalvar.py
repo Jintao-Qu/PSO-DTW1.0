a611470a755ae3f067e12cbad9206aa27f2f2f69
Jintao_Qu <00002@protonmail.com>
2018-11-29 20:01:08 +0800

    Update globalvar.py

--- code/globalvar.py
+++ code/globalvar.py
@@ -1,7 +1,21 @@
 def _init():
     global _global_dict
     _global_dict = {}
-
+    _global_dict["failename"] = "data/carcount.txt"
+    _global_dict["CRAZY_PSO"] = 0.1  # 2e-3
+    _global_dict["pop_size"] = 16
+    _global_dict["max_evaluations"] = 4800
+    _global_dict["IF_Elite"] = True
+    _global_dict["Elite_list"] = []
+    _global_dict["t_lastupdate"] = 0
+    _global_dict["t_updated"] = 0
+    _global_dict["TCONV"] = 100
+    _global_dict["gbest"] = 0x3f3f3f3f
+    _global_dict["Elite_list"] = []
+    _global_dict["FORCE_NOT_OVERLAP"] = True
+    _global_dict["DTW_ALGO"] = "Pierre_DTW" # CUSTOM_DTW, Pierre_DTW
+    _global_dict["SHOW_CONVERGENCE_RATE"] = True
+    _global_dict["scr_picfilename"] = "pic/pic_carcount.txt"
 def set_value(name, value):
     _global_dict[name] = value
 
