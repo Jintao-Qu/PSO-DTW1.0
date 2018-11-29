29969927e8d49e78f86efad2a1440684f93ba81b
Jintao_Qu <00002@protonmail.com>
2018-11-29 20:18:54 +0800

    Update __run.py

--- code/__run.py
+++ code/__run.py
@@ -2,22 +2,18 @@ from main import *
 import globalvar as gl
 import utils
 
-
+gl._init()
 for i in range(10):
-    filename = "data/carcount.txt"
-    wmin = 5
-    wmax = 7
-    gl._init()
+    filename = gl.get_value("filename")
     if(gl.get_value("SHOW_CONVERGENCE_RATE")):
         picfilename = gl.get_value("scr_picfilename")
         f = open(picfilename, "w")
         gl.set_value("f_show_convergence_rate", f)
 
-    main(filename=filename, wmin=wmin, wmax=wmax,
+    main(filename=filename, wmin=gl.get_value("wmin"), wmax=gl.get_value("wmax"),
          pop_size=gl.get_value("pop_size"), max_evaluations=gl.get_value("max_evaluations"), display=True)
 
     if(gl.get_value("SHOW_CONVERGENCE_RATE")):
-      utils.show_convergence_rate()
-
+        utils.show_convergence_rate()
 
 
