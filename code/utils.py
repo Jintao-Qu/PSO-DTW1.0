3069171f6ef8b360782607265da3f1cc568d9a73
Jintao_Qu <00002@protonmail.com>
2018-11-30 15:32:12 +0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,7 @@ import math
 import config
 from pylab import *
 import matplotlib.pyplot as plt
+import math
 def load_txt(filename):
     v = []
     file = open(filename, 'r')
@@ -56,3 +57,34 @@ def show_swarm_distribution():
     plt.show()
     #plt.scatter(Wi, Wj)
     #plt.show()
+
+def update_chaos_seed_logistic():
+    CHAOS_SEED = config.get_value("CHAOS_SEED")
+    for i in range(len(CHAOS_SEED)):
+        CHAOS_SEED[i] = CHAOS_SEED[i]*4.0*(1-CHAOS_SEED[i])
+    config.set_value("CHAOS_SEED", CHAOS_SEED)
+def update_chaos_seed_cube():
+    CHAOS_SEED = config.get_value("CHAOS_SEED")
+    for i in range(len(CHAOS_SEED)):
+        CHAOS_SEED[i] = 4.0*math.pow(CHAOS_SEED[i], 3) - 3.0*CHAOS_SEED[i]
+    config.set_value("CHAOS_SEED", CHAOS_SEED)
+
+def CHAOS_INIT():
+    seeds = []
+    for i in range(config.get_value("pop_size")):
+        CHAOS_SEED = config.get_value("CHAOS_SEED")
+        wmin = config.get_value("wmin")
+        wmax = config.get_value("wmax")
+        len = config.get_value("series_length")
+        tw1 = int(wmin + CHAOS_SEED[1]*(wmax-wmin))
+        tw2 = int(wmin + CHAOS_SEED[3]*(wmax-wmin))
+        twi = int((len - tw1)*CHAOS_SEED[0])
+        twj = int((len - tw2)*CHAOS_SEED[2])
+        seeds.append([twi, tw1, twj, tw2])
+        if config.get_value("CHAOS_ALGO") == "logistic":
+            update_chaos_seed_logistic()
+        if config.get_value("CHAOS_ALGO") == "cube":
+            update_chaos_seed_cube()
+
+    return seeds
+
