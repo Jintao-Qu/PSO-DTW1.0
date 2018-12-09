ce7ad266dff1a18c1dcdca8139c43450eddd1658
Jintao_Qu <00002@protonmail.com>
2018-12-09 17:19:32 +0800

    Update config.py

--- code/config.py
+++ code/config.py
@@ -3,7 +3,7 @@ def _init():
     _config_dict = {}
     _config_dict["filename"] = "data/carcount.txt"#dowjones, eog, carcount
     _config_dict["pop_size"] = 16
-    _config_dict["max_evaluations"] = 1600
+    _config_dict["max_evaluations"] = 4800
     _config_dict["wmin"] = 5
     _config_dict["wmax"] = 7
     _config_dict["gbest"] = 0x3f3f3f3f
@@ -20,12 +20,12 @@ def _init():
     _config_dict["FORCE_NOT_OVERLAP"] = True
     _config_dict["DTW_ALGO"] = "CUSTOM_DTW"# CUSTOM_DTW, Pierre_DTW
 
-    _config_dict["SHOW_CONVERGENCE_RATE"] = True
+    _config_dict["SHOW_CONVERGENCE_RATE"] = False
     _config_dict["CONVERGENCE_RATE_LIST"] = []
 
-    _config_dict["SHOW_SWARM_DISTRIBUTION"] = True
-    _config_dict["SHOW_INIT_SWARM"] = True
-    _config_dict["SHOW_SWARM_CYCLE"] = 30
+    _config_dict["SHOW_SWARM_DISTRIBUTION"] = False
+    _config_dict["SHOW_INIT_SWARM"] = False
+    _config_dict["SHOW_SWARM_CYCLE"] = 0x3f3f3f3f
     _config_dict["Xi"] = []
     _config_dict["Xj"] = []
     _config_dict["Wi"] = []
